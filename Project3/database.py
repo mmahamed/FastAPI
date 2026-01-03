581bca08443a459067287405a6689a9591998947
mmahamed <mmahamed@gmail.com>
2026-01-03 14:45:06 +0330

    postgres url

--- Project3/database.py
+++ Project3/database.py
@@ -8,7 +8,10 @@ from sqlalchemy.orm import declarative_base
 #                        'check_same_thread': False})
 
 # PostgreSQL URL for production deployment
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:09127585948@localhost/TodoApplicationDatabase'
+
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:09127585948@localhost/TodoApplicationDatabase'
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://fastapi_todo_databse_user:FvNJenrA9WFg7akmvYHAjTAK7oIAmMz8@dpg-d5cfiiogjchc73cmkrc0-a.virginia-postgres.render.com/fastapi_todo_databse'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
